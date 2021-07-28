b633f16c52c5ca200daf5c6b2b3fb44973ed3b70
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-07-28 11:21:27 -0700

    finish category card design

---
+++ src/components/basics/CategoryCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="categoryType" class="category_link">
+    <div class="category_card_wraper">
+      <img :src="categoryImg" alt="Ctegory Image">
+
+      <div class="overlay">
+        <button> {{categoryTxet}} </button>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ["categoryType", "categoryImg", "categoryTxet"],
+
+  mounted() {
+    console.log(this.categoryType);
+  }
+}
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,55 @@
 <template>
-  <TheSlider :sliderItems="sliderItems"/>
+  <div class="home">
+    <!-- START:: SLIDER -->
+    <TheSlider :sliderItems="sliderItems"/>
+    <!-- END:: SLIDER -->
+
+    <!-- START:: CATEGORIES -->
+    <div class="categories">
+      <div class="container">
+        <!-- START:: TITLE -->
+        <div class="header_wraper">
+          <h2> Categories </h2>
+          <span class="title_backdrop"> Categories </span>
+        </div>
+        <!-- START:: TITLE -->
+
+        <div class="row justify-content-center align-items-center">
+          <!-- START:: CATEGORY CAR -->
+          <div 
+            class="col-10 col-md-3"
+            v-for=" category in Categories "
+            :key=" category.id "
+          >
+            <CategoryCard
+              :categoryType=" category.type "
+              :categoryImg=" category.img "
+              :categoryTxet=" category.text "
+            />
+          </div>
+          <!-- END:: CATEGORY CAR -->
+        </div>
+      </div>
+    </div>
+    <!-- END:: CATEGORIES -->
+  </div>
 </template>
 
 <script>
 import TheSlider from "../components/layouts/TheSlider.vue";
+import CategoryCard from "../components/basics/CategoryCard.vue";
 
 export default {
   name: "Home",
 
   components: {
-    TheSlider
+    TheSlider,
+    CategoryCard
   },
 
   data() {
     return{
+      // START:: SLIDER DATA
       sliderItems: [
         {
           src: 'https://d-themes.com/vue/riode/demo-1/images/home/slides/slide1.jpg',
@@ -25,6 +61,37 @@ export default {
           src: 'https://d-themes.com/vue/riode/demo-diamart/images/home/slides/1.jpg',
         }
       ],
+      // END:: SLIDER DATA
+
+
+      // START:: CATEGORIES CARD DATA
+      Categories: [
+        {
+          id: 1,
+          type: "mens",
+          img: "https://d-themes.com/vue/riode/demo-1/images/categories/category1.jpg",
+          text: "For Men's",
+        },
+        {
+          id: 2,
+          type: "accessories",
+          img: "https://d-themes.com/vue/riode/demo-1/images/categories/category2.jpg",
+          text: "Accessories",
+        },
+        {
+          id: 3,
+          type: "women",
+          img: "https://d-themes.com/vue/riode/demo-1/images/categories/category3.jpg",
+          text: "Fashionable Women's",
+        },
+        {
+          id: 4,
+          type: "cosmetic",
+          img: "https://d-themes.com/vue/riode/demo-1/images/categories/category4.jpg",
+          text: "Cosmetic",
+        },
+      ],
+      // END:: CATEGORIES CARD DATA
     }
   }
 }
