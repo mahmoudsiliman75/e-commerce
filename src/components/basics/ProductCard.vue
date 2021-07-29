e042547e30b884176b2adefbf99a870f342cb897
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-07-28 17:14:09 -0700

    finish product comp witout details modal

---
+++ src/components/basics/ProductCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="wraper">
+    <div class="product_card_wraper">
+      <div class="product_image_wraper">
+        <div class="badges_wraper">
+          <span 
+            class="badge"
+            v-for=" badge in productData.badges "
+            :key=" badge "
+          >
+            {{ badge }}
+          </span>
+        </div>
+
+        <div class="overlay">
+          <div class="actions_btns_wraper">
+            <router-link to="/" v-b-tooltip.hover title="Details"> 
+              <ArrowRightIcon size="1.2x" class="arrow_icon"/>
+            </router-link>
+
+            <button class="add_to_wish" v-b-tooltip.hover title="Add To Wishlist">
+              <HeartIcon size="1.2x" class="heart_icon"/>
+            </button>
+
+            <button class="add_to_wish" v-b-tooltip.hover title="Add To Cart">
+              <ShoppingCartIcon size="1.2x" class="cart_icon"/>
+            </button>
+          </div>
+
+          <div class="bottom_button">
+            <b-button v-b-modal="'product_modal'">QUICK VIEW</b-button>
+          </div>
+        </div>
+        <img :src="productData.img_1" alt="Product Image">
+        <img :src="productData.img_2" alt="Product Image">
+      </div>
+
+      <div class="product_card_body">
+        <p class="category"> {{ productData.categoryName }} </p>
+        <h5 class="product_name"> {{ productData.productName.length > 33 ? productData.productName.substring(0,30)+".." : productData.productName }} </h5>
+        <h4 class="product_price" v-if="productData.descount.length != 0"> 
+          <span class="previous_price">
+            <span>{{ productData.price }}</span> 
+            <span>EGP</span> 
+          </span>
+
+          <span class="current_price">
+            <span>{{ productData.descount }}</span> 
+            <span>EGP</span> 
+          </span>
+        </h4>
+
+        <h4 class="product_price" v-else> 
+          <span class="current_price">
+            <span>{{ productData.price }}</span> 
+            <span>EGP</span> 
+          </span>
+        </h4>
+        <div class="rating">
+          <RatingStars :rate="productData.rate"/>
+          <div class="reviews"> ( {{productData.reviws}} Reviews ) </div>
+        </div>
+      </div>
+    </div>
+
+    <b-modal id="product_modal" class="product_modal" size="xl" hide-header hide-footer>
+      <b-button class="mt-3" variant="outline-danger" block @click="$bvModal.hide('product_modal')">Close Me</b-button>
+      <h1> Test </h1>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import { ArrowRightIcon, HeartIcon, ShoppingCartIcon, XIcon } from "vue-feather-icons";
+import RatingStars from "../ui/RatingStars.vue";
+
+export default {
+  components: {
+    ArrowRightIcon,
+    RatingStars,
+    HeartIcon,
+    ShoppingCartIcon,
+    XIcon
+  },
+
+  props: ["productData"],
+
+  computed() {
+    
+  }
+}
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <!-- START:: SLIDER -->
-    <TheSlider :sliderItems="sliderItems"/>
+    <TheSlider :sliderItems="sliderItems" />
     <!-- END:: SLIDER -->
 
     <!-- START:: CATEGORIES -->
@@ -9,22 +9,22 @@
       <div class="container">
         <!-- START:: TITLE -->
         <div class="header_wraper">
-          <h2> Categories </h2>
+          <h2>Categories</h2>
           <span class="title_backdrop"> Categories </span>
         </div>
         <!-- START:: TITLE -->
 
         <div class="row justify-content-center align-items-center">
           <!-- START:: CATEGORY CAR -->
-          <div 
+          <div
             class="col-10 col-md-3"
-            v-for=" category in Categories "
-            :key=" category.id "
+            v-for="category in Categories"
+            :key="category.id"
           >
             <CategoryCard
-              :categoryType=" category.type "
-              :categoryImg=" category.img "
-              :categoryTxet=" category.text "
+              :categoryType="category.type"
+              :categoryImg="category.img"
+              :categoryTxet="category.text"
             />
           </div>
           <!-- END:: CATEGORY CAR -->
@@ -32,37 +32,85 @@
       </div>
     </div>
     <!-- END:: CATEGORIES -->
+
+    <!-- START:: BEST SELLERS -->
+    <div class="best_sellers">
+      <div class="container">
+        <!-- START:: TITLE -->
+        <div class="header_wraper">
+          <h2>Best Sellers</h2>
+          <span class="title_backdrop"> Best Sellers </span>
+        </div>
+        <!-- START:: TITLE -->
+
+        <VueSlickCarousel v-bind="settings">
+          <!-- START:: PRODUCT CARD -->
+          <ProductCard
+            v-for="product in bestSellers"
+            :key=" product.id "
+            :productData=" product "
+          />
+          <!-- END:: PRODUCT CARD -->
+
+          <!-- START:: PRODUCT CARD -->
+          <!-- <ProductCard/> -->
+          <!-- END:: PRODUCT CARD -->
+
+          <!-- START:: PRODUCT CARD -->
+          <!-- <ProductCard/> -->
+          <!-- END:: PRODUCT CARD -->
+
+          <!-- START:: PRODUCT CARD -->
+          <!-- <ProductCard/> -->
+          <!-- END:: PRODUCT CARD -->
+
+          <!-- START:: PRODUCT CARD -->
+          <!-- <ProductCard/> -->
+          <!-- END:: PRODUCT CARD -->
+          
+        </VueSlickCarousel>
+      </div>
+    </div>
+    <!-- END:: BEST SELLERS -->
   </div>
 </template>
 
 <script>
+// START:: IMPORTING SLICK CAOUSIL
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+// END:: IMPORTING SLICK CAOUSIL
+
 import TheSlider from "../components/layouts/TheSlider.vue";
 import CategoryCard from "../components/basics/CategoryCard.vue";
+import ProductCard from "../components/basics/ProductCard.vue";
 
 export default {
   name: "Home",
 
   components: {
+    VueSlickCarousel,
     TheSlider,
-    CategoryCard
+    CategoryCard,
+    ProductCard,
   },
 
   data() {
-    return{
+    return {
       // START:: SLIDER DATA
       sliderItems: [
         {
-          src: 'https://d-themes.com/vue/riode/demo-1/images/home/slides/slide1.jpg',
+          src: "https://d-themes.com/vue/riode/demo-1/images/home/slides/slide1.jpg",
         },
         {
-          src: 'https://d-themes.com/vue/riode/demo-1/images/home/slides/slide2.jpg',
+          src: "https://d-themes.com/vue/riode/demo-1/images/home/slides/slide2.jpg",
         },
         {
-          src: 'https://d-themes.com/vue/riode/demo-diamart/images/home/slides/1.jpg',
-        }
+          src: "https://d-themes.com/vue/riode/demo-diamart/images/home/slides/1.jpg",
+        },
       ],
       // END:: SLIDER DATA
-
 
       // START:: CATEGORIES CARD DATA
       Categories: [
@@ -92,7 +140,83 @@ export default {
         },
       ],
       // END:: CATEGORIES CARD DATA
-    }
-  }
-}
+
+      // START:: BEST SELLERS DATA
+      bestSellers: [
+        {
+          id: 1,
+          img_1: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_7_1_98644502e2.jpg",
+          img_2: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_7_2_49cc0be0ad.jpg",
+          badges: ["TOP", "SALE"],
+          categoryName: "For Men's",
+          productName: "Beyond Riode Original T-Shirt",
+          price: "500",
+          descount: "250",
+          rate: 4,
+          reviws: 2,
+        },
+        {
+          id: 2,
+          img_1: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_2_1_9ae072548f.jpg",
+          img_2: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_2_2_7f087d1140.jpg",
+          badges: ["NEW"],
+          categoryName: "Accessories",
+          productName: " Mackintosh Poket backpack ",
+          price: "500",
+          descount: "",
+          rate: 2.5,
+          reviws: 4,
+        },
+        {
+          id: 3,
+          img_1: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_1_2_f77ebc3ad7.jpg",
+          img_2: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_1_1_67ca70354d.jpg",
+          badges: ["SALE"],
+          categoryName: "Fashionable Women's",
+          productName: " Solid pattern in fashion summer dress ",
+          price: "700",
+          descount: "400",
+          rate: 3.5,
+          reviws: 10,
+        },
+        {
+          id: 4,
+          img_1: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_12_1_8812a927b7.jpg",
+          img_2: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_12_2_b8a6d64d23.jpg",
+          badges: ["NEW"],
+          categoryName: "For Men's",
+          productName: " Converse blue training shoes ",
+          price: "600",
+          descount: "",
+          rate: 0,
+          reviws: 0,
+        },
+        {
+          id: 5,
+          img_1: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_17_1_7d420d81e9.jpg",
+          img_2: "https://d-themes.com/vue/riode/server/uploads/demo_1_product_17_2_829e54f8cc.jpg",
+          badges: ["TOP", "NEW"],
+          categoryName: "Accessories",
+          productName: " Fashionable Overnight Bag ",
+          price: "1100",
+          descount: "",
+          rate: 4.5,
+          reviws: 23,
+        },
+      ],
+      // END:: BEST SELLERS DATA
+
+      settings: {
+        dots: true,
+        arrows: true,
+        dotsClass: "slick-dots custom-dot-class",
+        edgeFriction: 0.35,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    };
+  },
+};
 </script>
